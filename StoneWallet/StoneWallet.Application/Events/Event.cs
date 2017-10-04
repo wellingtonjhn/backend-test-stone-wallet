0f8d1c14653037eacc43f638abadea6b00fed9b8
Wellington Nascimento <wellington.jhn@gmail.com>
2017-10-04 00:07:48 -0300

    Remove dependencia do NewtonsoftJson no Dominio

--- StoneWallet/StoneWallet.Application/Events/Event.cs
+++ StoneWallet/StoneWallet.Application/Events/Event.cs
@@ -1,15 +1,11 @@
 ﻿using MediatR;
-using Newtonsoft.Json;
 using System;
 
 namespace StoneWallet.Application.Events
 {
     public abstract class Event<TEvent> : INotification
     {
-        [JsonProperty(Order = 1)]
         public string EventName = typeof(TEvent).Name;
-
-        [JsonProperty(Order = 2)]
         public DateTime EventDate { get; } = DateTime.Now;
     }
 }
